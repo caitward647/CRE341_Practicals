5f45d361a6f4658bef053b0dc7d6b7eac73ee09f
caitward647 <ward-c56@ulster.ac.uk>
2025-03-20 14:17:04 +0000

    RatNPC
    runs away and wanders

--- Assets/_CRE341/Caitlin/Scripts/CaptureCount.cs
+++ Assets/_CRE341/Caitlin/Scripts/CaptureCount.cs
@@ -9,7 +9,7 @@ public class CaptureCount : MonoBehaviour
     public GameObject CageFloor;
     void Start()
     {
-        Debug.Log(" UI False");
+        //Debug.Log(" UI False");
         UiText.SetActive(true);
     }
 
@@ -17,7 +17,7 @@ public class CaptureCount : MonoBehaviour
     {
       if (other.gameObject.tag == "CageFloor")
         {
-            Debug.Log("npc enter cage");
+            //Debug.Log("npc enter cage");
             NPCManager.instance.npcCount++;
         }
     }
@@ -26,7 +26,7 @@ public class CaptureCount : MonoBehaviour
     {
         if (other.gameObject.tag == "CageFloor")
         {
-            Debug.Log("npc exit cage");
+           // Debug.Log("npc exit cage");
             NPCManager.instance.npcCount--;
         }
     }
--- Assets/_CRE341/Caitlin/Scripts/NPCRunAwayFromPlayer.cs
+++ Assets/_CRE341/Caitlin/Scripts/NPCRunAwayFromPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine.AI;
 public class NPCRunAwayFromPlayer : MonoBehaviour
 {
     public float speed = 30f;
-    public float safeDistance = 8f;
+    public float safeDistance;
     public float rotationSpeed = 20f;
 
     private Transform player;
@@ -25,63 +25,107 @@ public class NPCRunAwayFromPlayer : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").transform;
         npcRigidbody = GetComponent<Rigidbody>();
 
-       //agent = GetComponent<NavMeshAgent>();
+        //agent = GetComponent<NavMeshAgent>();
         //  _isWandering = true;
 
         //RunAwayFromPlayer();
+
+     //   ratBehaviour();
     }
 
     void Update()
     {
-     RunAwayFromPlayer();
+     ratBehaviour();
+    // RunAwayFromPlayer();
 
-       //if (_isWandering)
+        //if (_isWandering)
         //{
-          //  _wanderTimer += Time.deltaTime;
+        //  _wanderTimer += Time.deltaTime;
 
-          //  if (_wanderTimer >= wanderInterval)
-          //  {
-                //  _wanderTimer = 0;
-            //    Wander();
-           // }
-       // }
+        //  if (_wanderTimer >= wanderInterval)
+        //  {
+        //  _wanderTimer = 0;
+        //    Wander();
+        // }
+        // }
     }
 
-    void RunAwayFromPlayer()
+    void ratBehaviour() //different method
     {
         if (player == null) return;
 
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        if (distanceToPlayer < safeDistance)
+        if ((distanceToPlayer  < safeDistance))
         {
-            Debug.Log("Running away from player");
-            // agent.enabled = false;
-            Vector3 directionAwayFromPlayer = (transform.position - player.position).normalized; //finds distance
-
-            Vector3 newPosition = transform.position + directionAwayFromPlayer * speed * Time.deltaTime;
-            npcRigidbody.MovePosition(newPosition);
-
-            //rotating when being chased. Looks away from player
-              Quaternion targetRotation = Quaternion.LookRotation(directionAwayFromPlayer);
-              transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            RunAwayFromPlayer();
+            
         }
-
-        else
+        else 
         {
             Wander();
         }
 
     }
 
+    void RunAwayFromPlayer()
+    {
+        //if (player == null) return;
+
+       // float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+
+
+       // if (distanceToPlayer < safeDistance)
+        {
+            Debug.Log("Running away from player");
+            // agent.enabled = false;
+            // Vector3 directionAwayFromPlayer = (transform.position - player.position).normalized; //finds distance
+
+            //  Vector3 newPosition = transform.position + directionAwayFromPlayer * speed * Time.deltaTime;
+            // npcRigidbody.MovePosition(newPosition);
+
+            //rotating when being chased. Looks away from player
+            // Quaternion targetRotation = Quaternion.LookRotation(directionAwayFromPlayer);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); //creating different movement with slerp.
+
+            // if (Vector3.Distance(transform.position, player.position) < safeDistance) //checking distance
+            // {
+            //     Vector3 directionToPlayer = (transform.position - player.position).normalized;
+            // }
+
+            {
+                Vector3 directionToPlayer = transform.position - player.position;
+                Vector3 runTo = transform.position + directionToPlayer.normalized * safeDistance;
+
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(runTo, out hit, safeDistance, 1))
+                {
+                    agent.SetDestination(hit.position);
+                }
+            }
+
+
+        }
+        //hate
+
+      //  if (distanceToPlayer > safeDistance)
+      //  {
+          // Wander();
+       // }
+
+    }
+
+    //wander
     void Wander()
     {
-        Debug.Log("Wandering");
-        agent = GetComponent<NavMeshAgent>();
-        //  agent.enabled = true;
-        if (player == null) return;
+            {
+            Debug.Log("Wandering");
+            agent = GetComponent<NavMeshAgent>();
+            //  agent.enabled = true;
+            if (player == null) return;
 
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
             Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
             randomDirection += transform.position;
@@ -91,5 +135,6 @@ public class NPCRunAwayFromPlayer : MonoBehaviour
             {
                 agent.SetDestination(hit.position);
             }
+        }
     }
 }
