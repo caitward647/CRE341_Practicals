42bc172787ca8a1492b314f574b6ba7caa742cdb
caitward647 <ward-c56@ulster.ac.uk>
2025-03-20 15:10:28 +0000

    code
    fixed organised

--- Assets/_CRE341/Caitlin/Scripts/NPCRunAwayFromPlayer.cs
+++ Assets/_CRE341/Caitlin/Scripts/NPCRunAwayFromPlayer.cs
@@ -13,41 +13,19 @@ public class NPCRunAwayFromPlayer : MonoBehaviour
     public NavMeshAgent agent;
     public float wanderRadius = 10f;
     public float wanderInterval = 10f;
-    // public float pauseTime = 2f;
 
     private float _wanderTimer;
     private bool _isWandering;
-
-    //WanderAI WanderingNPC;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         npcRigidbody = GetComponent<Rigidbody>();
-
-        //agent = GetComponent<NavMeshAgent>();
-        //  _isWandering = true;
-
-        //RunAwayFromPlayer();
-
-     //   ratBehaviour();
     }
 
     void Update()
     {
      ratBehaviour();
-    // RunAwayFromPlayer();
-
-        //if (_isWandering)
-        //{
-        //  _wanderTimer += Time.deltaTime;
-
-        //  if (_wanderTimer >= wanderInterval)
-        //  {
-        //  _wanderTimer = 0;
-        //    Wander();
-        // }
-        // }
     }
 
     void ratBehaviour() //different method
@@ -70,71 +48,36 @@ public class NPCRunAwayFromPlayer : MonoBehaviour
 
     void RunAwayFromPlayer()
     {
-        //if (player == null) return;
-
-       // float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-
-
-
-       // if (distanceToPlayer < safeDistance)
         {
-          //  Debug.Log("Running away from player");
-            // agent.enabled = false;
-            // Vector3 directionAwayFromPlayer = (transform.position - player.position).normalized; //finds distance
+             Vector3 directionToPlayer = transform.position - player.position;
+             Vector3 runTo = transform.position + directionToPlayer.normalized * safeDistance;
 
-            //  Vector3 newPosition = transform.position + directionAwayFromPlayer * speed * Time.deltaTime;
-            // npcRigidbody.MovePosition(newPosition);
-
-            //rotating when being chased. Looks away from player
-            // Quaternion targetRotation = Quaternion.LookRotation(directionAwayFromPlayer);
-            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); //creating different movement with slerp.
-
-            // if (Vector3.Distance(transform.position, player.position) < safeDistance) //checking distance
-            // {
-            //     Vector3 directionToPlayer = (transform.position - player.position).normalized;
-            // }
-
-            {
-                Vector3 directionToPlayer = transform.position - player.position;
-                Vector3 runTo = transform.position + directionToPlayer.normalized * safeDistance;
-
-                NavMeshHit hit;
+             NavMeshHit hit;
                 if (NavMesh.SamplePosition(runTo, out hit, safeDistance, 1))
                 {
                     agent.SetDestination(hit.position);
                 }
-            }
-
-
-        }
-        //hate
-
-      //  if (distanceToPlayer > safeDistance)
-      //  {
-          // Wander();
-       // }
-
+        } 
     }
 
     //wander
     void Wander()
     {
-            {
-           // Debug.Log("Wandering");
-            agent = GetComponent<NavMeshAgent>();
-            //  agent.enabled = true;
-            if (player == null) return;
+      {
+       // Debug.Log("Wandering");
+       agent = GetComponent<NavMeshAgent>();
+       if (player == null) return;
 
-         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+             float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-            Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
-            randomDirection += transform.position;
+             Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
+             randomDirection += transform.position;
 
             NavMeshHit hit;
-            if (NavMesh.SamplePosition(randomDirection, out hit, wanderRadius, NavMesh.AllAreas))
-            {
-                agent.SetDestination(hit.position);
-            }
-        }
+              if (NavMesh.SamplePosition(randomDirection, out hit, wanderRadius, NavMesh.AllAreas))
+              {
+                     agent.SetDestination(hit.position);
+              }
+      }
     }
 }
